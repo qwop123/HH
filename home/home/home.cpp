cdf4365825239939c6188917d23d361282f148b5
suengwoo Lee <suengwoo915@naver.com>
2021-04-01 01:15:55 +0900

    랜덤 배열

--- home/home/home.cpp
+++ home/home/home.cpp
@@ -1,127 +1,319 @@
 ﻿#include <stdio.h>
-//맵 데이터
-#define MAP_Y 10
-#define MAP_X 10
-#define WALL 1
-#define ROAD 0
-#define GEM 2
-//이동관련
-#define UP 1
-#define DOWN 2
-#define RIGHT 3
-#define LEFT 4
+#include <stdlib.h>
+#include <time.h>
+
+#define S 0 // 가위
+#define R 1 // 바위
+#define P 2 // 보
 
 int main()
+
 {
-	int map[MAP_Y][MAP_X] = {
-		{0,1,1,1,1,1,1,1,1,1},
-		{0,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,0,1},
-		{1,0,0,0,0,0,1,1,0,1},
-		{1,0,0,2,0,0,1,1,0,1},
-		{1,1,1,1,1,0,1,1,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,0,0,0,0,0,0,0,0,1},
-		{1,1,1,1,1,1,1,1,1,1},
-	};
-	int playerY = 0, playerX = 0;
-	int select = 0;
-	while (1)
+
+	int com; // 컴퓨터가 낸 값을 저장합니다. 
+
+	int user; // 사용자가 낸 값을 저장합니다. 
+
+	int result[3] = { 0, }; // 게임의 결과를 저장합니다. 
+
+	// result 배열의 모든 요소를 0으로 초기화합니다. 
+
+	int cnt = 0; // 게임의 횟수를 저장합니다. 0으로 초기화합니다. 
+
+	srand((unsigned)time(NULL)); // rand 함수의 결과가 매번 다르도록 시드값을 지정합니다. 
+
+	while (1) // 사용자가 종료할 때 까지 반복합니다. 
+
 	{
-		for (int i = 0; i < MAP_Y; i++)
+
+		com = rand() % 3 + 1; // 1~3 값 중 하나를 저장합니다. 
+
+		printf("**************** 가위 바위 보 게임 ****************\n");
+
+		printf("(1. 가위 _ 2. 바위 _ 3. 보 _ 0. 종료)\n\n");
+
+		printf("입력해주세요 : ");
+
+		scanf_s("%d", &user);
+
+		if (user > 0 && user < 4) // 사용자가 1~3 값을 입력했을 경우입니다. 
+
 		{
-			for (int j = 0; j < MAP_X; j++)
-			{
-				if (map[i][j] == ROAD) // ROAD안에 플레이어추가
-				{
-					if (playerY == i && playerX == j)
-					{
-						printf("♀");
-					}
-					else
-						printf("□");
-				}
-				else if (map[i][j] == WALL)
-				{
-					printf("■");
-				}
-				else if (map[i][j] == GEM)
-				{
-					printf("◈");
-				}
-			}
-			printf("\n");
-		}
-		printf("1.위 2.아래 3.오른쪽 4.왼쪽 5.종료\n");
-		scanf("%d", &select);
 
-		switch (select)
+			cnt++; // 정상적인 입력의 경우 게임 횟수를 증가시킵니다. 
+
+			// 컴퓨터, 사용자가 낸 값에 따라 가위, 바위, 보 중 출력할 값을 선택합니다. 
+
+			printf("\n컴퓨터 : %s\n", (com == 1 ? "가위" : com == 2 ? "바위" : "보"));
+
+			printf("사용자 : %s\n\n", (user == 1 ? "가위" : user == 2 ? "바위" : "보"));
+
+			if (com == user) // 컴퓨터의 값과 사용자의 값이 같으면 비기는 경우입니다. 
+
+			{
+
+				printf("후.. 비겼습니다. \n\n");
+
+				result[1]++; // 무승부 횟수를 증가시킵니다. 
+
+			}// 지는 경우의 수입니다.  
+
+			else if (((com == 1) && (user == 3)) ||
+
+				((com == 2) && (user == 1)) ||
+
+				((com == 3) && (user == 2)))
+
+			{
+
+				printf("졌습니다.ㅠㅠ\n\n");
+
+				result[2]++; // 패배 횟수를 증가시킵니다. 
+
+			}
+
+			else // 나머지는 이긴 경우입니다. 
+
+			{
+
+				printf("ㅋㅋㅋ이겼습니다.\n\n");
+
+				result[0]++; // 승리 횟수를 증가시킵니다. 
+
+			}
+
+			printf("%d전 %d승 %d무 %d패\n\n", cnt, result[0], result[1], result[2]);
+
+		}
+
+		else // 사용자가 입력한 값이 1, 2, 3이 아닌 경우 
+
 		{
-		case UP:
-			if (playerY - 1 >= 0)
-			{
-				if (map[playerY - 1][playerX] == ROAD)
-				{
-					playerY--;
-				}
-				else if (map[playerY - 1][playerX] == GEM)
-				{
-					printf("축하합니다! 보석을 찾았습니다.\n");
-					playerX = 0, playerY = 0;
 
-				}
-			}
-			break;
-		case DOWN:
-			if (playerY + 1 < MAP_Y)
-			{
-				if (map[playerY + 1][playerX] == ROAD)
-				{
-					playerY++;
-				}
-				else if (map[playerY + 1][playerX] == GEM)
-				{
-					printf("축하합니다! 보석을 찾았습니다.\n");
-					playerX = 0, playerY = 0;
+			if (user == 0) // 사용자가 입력한 값이 0이면 종료합니다. 
 
-				}
-			}
-			break;
-		case RIGHT:
-			if (playerX + 1 < MAP_X)
 			{
-				if (map[playerY][playerX+1] == ROAD)
-				{
-					playerX++;
-				}
-				else if (map[playerY][playerX+1] == GEM)
-				{
-					printf("축하합니다! 보석을 찾았습니다.\n");
-					playerX = 0, playerY = 0;
 
-				}
-			}
-			break;
-		case LEFT:
-			if (playerX - 1 >= 0)
-			{
-				if (map[playerY][playerX - 1] == ROAD)
-				{
-					playerX--;
-				}
-				else if (map[playerY][playerX - 1] == GEM)
-				{
-					printf("축하합니다! 보석을 찾았습니다.\n");
-					playerX = 0, playerY = 0;
-				}
-			}
-			break;
-		default:
-			break;
+				printf("게임을 종료합니다.\n");
+
+				break; // 종료 
+
+			} // 그 외에는 예외처리 메세지 출력 
+
+			printf("숫자를 제대로 입력해주세요\n\n");
+
 		}
+
+		system("pause"); // 일시정지 아무키나 누르면 진행됩니다. 
+
+		system("cls"); // 화면 내용을 모두 지웁니다. 
+
 	}
+
+	return 0;
+
 }
+
+
+	/*int main()
+	{
+		int game[3] = {0,1,2};
+		int select = 0;
+		int win = 0;
+		int lose = 0;
+		int draw = 0;
+		int key = 0;		
+		while (true)
+		{
+			srand((int)time(NULL));
+			printf("0.가위 1.바위 2.보 -> ");
+			scanf("%d", &select);
+			if (select >= 3)
+			{
+				printf("다시 입력하세요!\n");				
+			}
+			else
+			printf("Player: %d\t", select);
+			
+			for (int i = 0; i < 1; i++)
+			{
+				game[i] = rand() % 3;
+				
+				if (select == game[i])
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Draw!\n");
+					draw++;
+				}
+				else if (select == 1 && game[i] == 0) // 컴터 가위
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Win!\n");
+					win++;
+				}
+				else if (select == 2 && game[i] == 0)
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Lose!\n");
+					lose++;
+				}
+				else if (select == 0 && game[i] == 1) // 컴터 바위
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Lose!\n");
+					lose++;
+				}
+				else if (select == 2 && game[i] == 1)
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Win!\n");
+					win++;
+				}
+				else if (select == 0 && game[i] == 2) // 컴터 보
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Win!\n");
+					win++;
+				}
+				else if (select == 1 && game[i] == 2)
+				{
+					printf("Com: %d\n", game[i]);
+					printf("Lose!\n");
+					lose++;
+				}
+				if (select < 3)
+				{
+					printf("Win:[%d]번,Lose[%d]번,Draw[%d]번\n\n", win, lose, draw);
+				}
+			}
+		}
+		
+			return 0;
+	}*/
+////맵 데이터
+//#define MAP_Y 10
+//#define MAP_X 10
+//#define WALL 1
+//#define ROAD 0
+//#define GEM 2
+////이동관련
+//#define UP 1
+//#define DOWN 2
+//#define RIGHT 3
+//#define LEFT 4
+//
+//int main()
+//{
+//	int map[MAP_Y][MAP_X] = {
+//		{0,1,1,1,1,1,1,1,1,1},
+//		{0,0,0,0,0,0,0,0,0,1},
+//		{1,1,1,1,1,1,1,1,0,1},
+//		{1,0,0,0,0,0,1,1,0,1},
+//		{1,0,0,2,0,0,1,1,0,1},
+//		{1,1,1,1,1,0,1,1,0,1},
+//		{1,0,0,0,0,0,0,0,0,1},
+//		{1,0,0,0,0,0,0,0,0,1},
+//		{1,0,0,0,0,0,0,0,0,1},
+//		{1,1,1,1,1,1,1,1,1,1},
+//	};
+//	int playerY = 0, playerX = 0;
+//	int select = 0;
+//	while (1)
+//	{
+//		for (int i = 0; i < MAP_Y; i++)
+//		{
+//			for (int j = 0; j < MAP_X; j++)
+//			{
+//				if (map[i][j] == ROAD) // ROAD안에 플레이어추가
+//				{
+//					if (playerY == i && playerX == j)
+//					{
+//						printf("♀");
+//					}
+//					else
+//						printf("□");
+//				}
+//				else if (map[i][j] == WALL)
+//				{
+//					printf("■");
+//				}
+//				else if (map[i][j] == GEM)
+//				{
+//					printf("◈");
+//				}
+//			}
+//			printf("\n");
+//		}
+//		printf("1.위 2.아래 3.오른쪽 4.왼쪽 5.종료\n");
+//		scanf("%d", &select);
+//
+//		switch (select)
+//		{
+//		case UP:
+//			if (playerY - 1 >= 0)
+//			{
+//				if (map[playerY - 1][playerX] == ROAD)
+//				{
+//					playerY--;
+//				}
+//				else if (map[playerY - 1][playerX] == GEM)
+//				{
+//					printf("축하합니다! 보석을 찾았습니다.\n");
+//					playerX = 0, playerY = 0;
+//
+//				}
+//			}
+//			break;
+//		case DOWN:
+//			if (playerY + 1 < MAP_Y)
+//			{
+//				if (map[playerY + 1][playerX] == ROAD)
+//				{
+//					playerY++;
+//				}
+//				else if (map[playerY + 1][playerX] == GEM)
+//				{
+//					printf("축하합니다! 보석을 찾았습니다.\n");
+//					playerX = 0, playerY = 0;
+//
+//				}
+//			}
+//			break;
+//		case RIGHT:
+//			if (playerX + 1 < MAP_X)
+//			{
+//				if (map[playerY][playerX+1] == ROAD)
+//				{
+//					playerX++;
+//				}
+//				else if (map[playerY][playerX+1] == GEM)
+//				{
+//					printf("축하합니다! 보석을 찾았습니다.\n");
+//					playerX = 0, playerY = 0;
+//
+//				}
+//			}
+//			break;
+//		case LEFT:
+//			if (playerX - 1 >= 0)
+//			{
+//				if (map[playerY][playerX - 1] == ROAD)
+//				{
+//					playerX--;
+//				}
+//				else if (map[playerY][playerX - 1] == GEM)
+//				{
+//					printf("축하합니다! 보석을 찾았습니다.\n");
+//					playerX = 0, playerY = 0;
+//				}
+//			}
+//			break;
+//		default:
+//			break;
+//		}
+//		system("cls");
+//	}
+//}
 
 // 2021.03.26 < 반복문 >
 
